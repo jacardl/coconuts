511d01790d324724972c0b2ff949625ad6bd07ed
Jac <u_jar@sina.com>
2016-06-07 16:19:02 +0800

    1. add desktop.ico 2. add setup.bat

---
+++ setup.bat
@@ -0,0 +1,3 @@
+cd C:\Python27\PyInstaller-2.1
+c:
+python pyinstaller.py  -D --noconsole --icon="D:\Python\coconuts\desktop.ico" --distpath="D:\Python\coconuts" --name="Coconuts" "D:\Python\coconuts\main.py"
